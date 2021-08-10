df40a5d18c51aca300647cfbe2b8628074d2fe72
Sakou <86302989+Sakou123@users.noreply.github.com>
2021-08-10 15:52:05 +0200

    Completed

--- mcu.sql
+++ mcu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 26 juil. 2021 à 14:19
+-- Généré le : mar. 10 août 2021 à 13:51
 -- Version du serveur :  5.7.31
 -- Version de PHP : 7.3.21
 
@@ -33,22 +33,22 @@ CREATE TABLE IF NOT EXISTS `acteur` (
   `nom` varchar(50) NOT NULL,
   `prenom` varchar(50) NOT NULL,
   `date` date NOT NULL,
-  `creation` datetime DEFAULT NULL,
-  `update` datetime DEFAULT NULL,
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `acteur`
 --
 
-INSERT INTO `acteur` (`id`, `nom`, `prenom`, `date`, `creation`, `update`) VALUES
-(1, 'Evans', 'Christopher', '1981-06-13', '2021-07-26 11:37:08', '2021-07-26 11:37:08'),
-(2, 'Downey Junior', 'Robert', '1965-04-04', '2021-07-26 11:38:17', '2021-07-26 11:38:17'),
-(3, 'Ruffalo', 'Mark', '1967-11-22', '2021-07-26 11:38:50', '2021-07-26 11:38:50'),
-(4, 'Hemsworth', 'Chris', '1983-08-11', '2021-07-26 11:39:23', '2021-07-26 11:39:23'),
-(5, 'Scarlett', 'Johansson', '1984-11-22', '2021-07-26 11:39:56', '2021-07-26 11:39:56'),
-(6, 'Hiddleston', 'Tom', '1981-02-09', '2021-07-26 11:40:26', '2021-07-26 11:40:26');
+INSERT INTO `acteur` (`id`, `nom`, `prenom`, `date`, `creation_date`, `update`) VALUES
+(1, 'Evans', 'Christopher', '1981-06-13', '2021-07-28 15:19:20', '2021-07-28 15:19:20'),
+(2, 'Downey Junior', 'Robert', '1965-04-04', '2021-07-28 15:19:20', '2021-07-28 15:19:20'),
+(3, 'Ruffalo', 'Mark', '1967-11-22', '2021-07-28 15:19:20', '2021-07-28 15:19:20'),
+(4, 'Hemsworth', 'Chris', '1983-08-11', '2021-07-28 15:19:20', '2021-07-28 15:19:20'),
+(5, 'Scarlett', 'Johansson', '1984-11-22', '2021-07-28 15:19:20', '2021-07-28 15:19:20'),
+(6, 'Hiddleston', 'Tom', '1981-02-09', '2021-07-28 15:19:20', '2021-07-28 15:19:20');
 
 -- --------------------------------------------------------
 
@@ -94,22 +94,22 @@ CREATE TABLE IF NOT EXISTS `film` (
   `realisateur` varchar(50) NOT NULL,
   `duree` time NOT NULL,
   `date` year(4) NOT NULL,
-  `creation` varchar(50) DEFAULT NULL,
-  `update` datetime DEFAULT NULL,
+  `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `update` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `film`
 --
 
-INSERT INTO `film` (`id`, `titre`, `realisateur`, `duree`, `date`, `creation`, `update`) VALUES
-(1, 'Avengers', 'Joss Whedon', '02:23:00', 2012, '2021-07-26 09:09:45', '2021-07-26 09:09:45'),
-(2, 'Captaine america : First avengers', 'Joe Johnston', '02:04:00', 2011, '2021-07-26 09:09:05', '2021-07-26 09:09:05'),
-(3, 'Iron Man', 'Jon Faveau', '02:06:00', 2008, '2021-07-26 09:04:40', '2021-07-26 09:04:40'),
-(4, 'Iron Man 2', 'Jon Faveau', '02:05:00', 2010, '2021-07-26 09:03:49', '2021-07-26 09:03:49'),
-(5, 'Iron Man 3', 'Shane Black', '02:10:00', 2013, '2021-07-26 09:10:24', '2021-07-26 09:10:24'),
-(6, 'L\'incroybale Hulk', 'Louis Leterrier', '01:52:00', 2008, '2021-07-26 09:05:30', '2021-07-26 09:05:30');
+INSERT INTO `film` (`id`, `titre`, `realisateur`, `duree`, `date`, `creation_date`, `update`) VALUES
+(1, 'Avengers', 'Joss Whedon', '02:23:00', 2012, '2021-07-28 14:52:49', '2021-07-28 14:52:49'),
+(2, 'Captaine america : First avengers', 'Joe Johnston', '02:04:00', 2011, '2021-07-28 14:52:49', '2021-07-28 14:52:49'),
+(3, 'Iron Man', 'Jon Faveau', '02:06:00', 2008, '2021-07-28 14:52:49', '2021-07-28 14:52:49'),
+(4, 'Iron Man 2', 'Jon Faveau', '02:05:00', 2010, '2021-07-28 14:52:49', '2021-07-28 14:52:49'),
+(5, 'Iron Man 3', 'Shane Black', '02:10:00', 2013, '2021-07-28 14:52:49', '2021-07-28 14:52:49'),
+(6, 'L\'incroybale Hulk', 'Louis Leterrier', '01:52:00', 2008, '2021-07-28 14:52:49', '2021-07-28 14:52:49');
 
 --
 -- Contraintes pour les tables déchargées
